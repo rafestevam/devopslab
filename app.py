0373549e6987ad9db0a3ac5952ed77fd2dbf0386
rafestevam <rafestevam@gmail.com>
2023-04-30 18:19:42 -0300

    Applying CSRF Protection to app

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "MIGUEL SALLES PINECIO"
+    return "Olá, Bem vindo ao mundo do DevSecOps com Sonar!"
 
 if __name__ == '__main__':
     app.run()
